0b82d41326e9cf7d50631a8a068a1f3cbf6b87cc
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-11-08 21:23:53 -0600

    Final Commit.

--- FinancialDDB/DepricationCalculator.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class DepricationCalculator
-
-    Public Shared Function GetDepricationValue(ByVal )
-
-        Return
-
-    End Function
-
-End Class
--- FinancialDDB/Main.vb
+++ FinancialDDB/Main.vb
@@ -7,7 +7,8 @@ Public Class Main
     Dim dblAssetCost As Double = 0
     Dim dblSalvageValue As Double = 0
     Dim dblUsefulLife As Double = 0
-
+    Dim strHeading As String = "Year     Deprication"
+    Dim dblDep As Double = 0
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
 
@@ -22,9 +23,16 @@ Public Class Main
         dblSalvageValue = txtSalvageValue.Text
         Double.TryParse(lstUsefulLife.SelectedItem.ToString, dblUsefulLife)
 
+        'Heading for the text box:
+        txtSchedule.Text = strHeading & ControlChars.NewLine
 
+        'Code modified from http://stackoverflow.com/questions/5613924/financial-ddb-function-vb-net
+        For Intterm As Integer = 1 To dblUsefulLife
+            dblDep = Financial.DDB(dblAssetCost, dblSalvageValue, dblUsefulLife, Intterm)
+            txtSchedule.Text = txtSchedule.Text & ControlChars.NewLine & Intterm.ToString & "            " _
+                & dblDep.ToString("c2")
 
-
+        Next
 
     End Sub
 
@@ -36,6 +44,23 @@ Public Class Main
         Next intUsefulYear
 
         'Selects number 0 onload:
+        lstUsefulLife.SelectedItem = "3"
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'Clears the text boxes, lables, and variables:
+        txtAssetCost.Text = String.Empty
+        txtSalvageValue.Text = String.Empty
+        txtSchedule.Text = String.Empty
+
+        dblAssetCost = 0
+        dblSalvageValue = 0
+        dblUsefulLife = 0
+        dblDep = 0
+
+        'Resets focus in the list box:
         lstUsefulLife.SelectedItem = "3"
 
     End Sub
